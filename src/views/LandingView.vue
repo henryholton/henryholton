8ff52c0a4dac32122abd4efdd33640a34d5616a1
Henry Holton <noway1526@gmail.com>
2026-06-01 02:02:20 -0400

    add basic LandingView layout/styles

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -2,7 +2,24 @@
 
 <template>
 	<main>
-		<h1 class="text-4xl">Apps and Projects</h1>
-		<RouterLink to="math-svg-generator"> click me </RouterLink>
+		<h1 class="mt-30 text-5xl text-center">Apps and Projects</h1>
+		<div
+			class="mt-10 grid grid-cols-2 mx-20 justify-items-center gap-x-4"
+		>
+			<a class="project-card" href="https://inkform.io">
+				Inkform
+			</a>
+			<RouterLink to="math-svg-generator" class="project-card">
+				Math Symbols SVG Generator</RouterLink
+			>
+		</div>
 	</main>
 </template>
+
+<style scoped>
+@reference "#index.css";
+
+.project-card {
+	@apply w-full border-2 border-accent rounded-xl overflow-hidden aspect-square flex justify-center items-center;
+}
+</style>
