28979c6aeaa599bb75c9eda6b57a0b57ad8b4b36
Henry Holton <noway1526@gmail.com>
2026-06-01 01:18:44 -0400

    set up routing

---
+++ src/views/LandingView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<main>
+		<RouterLink to="math-svg-generator"> click me </RouterLink>
+	</main>
+</template>
---
+++ src/views/MathSVGGenerator.vue
@@ -0,0 +1,5 @@
+<template>
+	<main>this will be the math SVG generator thingy</main>
+</template>
+
+<style></style>
