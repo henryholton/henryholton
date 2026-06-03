2031ea5755160343143419ef616032d402980889
Henry Holton <noway1526@gmail.com>
2026-06-03 00:23:04 -0400

    add the start of TexToSVG generator

--- src/views/MathSVGGenerator.vue
+++ src/views/MathSVGGenerator.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { loadCDN } from "@/utils/loadCDN"
+import { onMounted, ref } from "vue"
+
+const output = ref<string>()
+
+onMounted(async () => {
+	window.MathJax = {
+		loader: {
+			load: ["input/tex", "output/svg"],
+		},
+	}
+
+	await loadCDN("https://cdn.jsdelivr.net/npm/mathjax@4/tex-svg.js")
+
+	const svgNode = window.MathJax.tex2svg?.("x^2")
+
+	console.log(svgNode)
+})
+</script>
 <template>
 	<main>this will be the math SVG generator thingy</main>
+	<div :v-html="output"></div>
 </template>
 
-<style></style>
+<style scoped></style>
